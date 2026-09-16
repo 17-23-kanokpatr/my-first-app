526edd957a6f99db5e9ab2eb19b5c9ed47a157ca
17-23-kanokpatr <56423@yupparaj.ac.th>
2026-09-16 11:32:18 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -18,8 +18,8 @@ if "ans3_val" not in st.session_state:
 def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
     st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
-    st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 3
-    st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 4
+    st.session_state.ans3_val = ""  # เคลียร์ค่าช่องข้อ 3
+    st.session_state.ans4_val = ""  # เคลียร์ค่าช่องข้อ 4
     st.session_state.start = time.time()  # เริ่มเวลาใหม่
     st.session_state.is_ended = False  # ปิด Dialog
 
@@ -34,8 +34,8 @@ def show_result_dialog(ans1, ans2):
 
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
-    u_ans1 = ans1.strip().lower()
-    u_ans2 = ans2.strip().lower()
+    u_ans3 = ans1.strip().lower()
+    u_ans4 = ans2.strip().lower()
 
 
     # ตรวจข้อ 1
